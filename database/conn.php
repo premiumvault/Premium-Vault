2fc40347478c6b4c2e28c664996775c3638681cd
premiumvault <65849868+premiumvault@users.noreply.github.com>
2020-06-25 22:26:49 +0530

    Update conn.php

--- database/conn.php
+++ database/conn.php
@@ -1,4 +1,4 @@
 <?php
 //database connection
-($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","","blog_admin_db"));  //host,user,password,database
+($GLOBALS["___mysqli_ston"] = mysqli_connect("sql12.freemysqlhosting.net,"sql12350701","Qp5ciSwIkw","sql12350701"));  //host,user,password,database
 ?>
