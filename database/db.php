904de294e986ca7f399c2a4ffa348928cf4d477f
premiumvault <65849868+premiumvault@users.noreply.github.com>
2020-06-25 22:28:04 +0530

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-	$database_username = 'root';//user name
-	$database_password = '';//password
-	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
+	$database_username = 'sql12350701';//user name
+	$database_password = 'Qp5ciSwIkw';//password
+	$pdo_conn = new PDO( 'mysql:host=sql12.freemysqlhosting.net;dbname=sql12350701', $database_username, $database_password );
 ?>
